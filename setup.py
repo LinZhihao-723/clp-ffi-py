7311bc99e75feaa0b6d2af39faea46f5532844ed
LittleStar <59785146+LinZhihao-723@users.noreply.github.com>
2024-03-16 20:41:33 +0800

    Enable parallel compilation

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
+import distutils.ccompiler
+import multiprocessing.pool
 import os
 import platform
 import sys
-from setuptools import setup, Extension
-from typing import List, Optional
+from typing import List, Optional, Tuple
+
+from setuptools import Extension, setup
 
 ir_native: Extension = Extension(
     name="clp_ffi_py.ir.native",
@@ -41,6 +44,35 @@ ir_native: Extension = Extension(
     define_macros=[("SOURCE_PATH_SIZE", str(len(os.path.abspath("./src/clp/components/core"))))],
 )
 
+
+def _parallel_compile(
+    self: distutils.ccompiler.CCompiler,
+    sources: List[str],
+    output_dir: Optional[str] = None,
+    macros: Optional[List[Tuple[str, Optional[str]]]] = None,
+    include_dirs: Optional[List[str]] = None,
+    debug: int = 0,
+    extra_preargs: Optional[List[str]] = None,
+    extra_postargs: Optional[List[str]] = None,
+    depends: Optional[List[str]] = None,
+) -> List[str]:
+    macros, objects, extra_postargs, pp_opts, build = self._setup_compile(
+        output_dir, macros, include_dirs, sources, depends, extra_postargs
+    )
+    cc_args = self._get_cc_args(pp_opts, debug, extra_preargs)
+
+    def _compile_single_file(obj: str) -> None:
+        try:
+            src, ext = build[obj]
+        except KeyError:
+            return
+        self._compile(obj, src, ext, cc_args, extra_postargs, pp_opts)
+
+    num_cores: int = multiprocessing.cpu_count()
+    multiprocessing.pool.ThreadPool(num_cores).map(_compile_single_file, objects)
+    return objects
+
+
 if "__main__" == __name__:
     try:
         if "Darwin" == platform.system():
@@ -49,17 +81,18 @@ if "__main__" == __name__:
             if None is target:
                 env_setup_needed = True
             else:
-                version_values: List[int] = [int(part) for part in target.split('.')]
+                version_values: List[int] = [int(part) for part in target.split(".")]
                 if 10 >= version_values[0] and 15 >= version_values[1]:
                     env_setup_needed = True
             if env_setup_needed:
                 os.environ["MACOSX_DEPLOYMENT_TARGET"] = "10.15"
 
+        distutils.ccompiler.CCompiler.compile = _parallel_compile
         project_name: str = "clp_ffi_py"
         description: str = "CLP FFI Python Interface"
         extension_modules: List[Extension] = [ir_native]
         if (3, 7) > sys.version_info:
-            sys.exit(f"The minimum Python version required is Python3.7")
+            sys.exit("The minimum Python version required is Python3.7")
         else:
             setup(
                 name=project_name,
